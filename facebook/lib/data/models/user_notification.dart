32761f3e3222050810c774186a937374b0dcbcb3
Haupham99 <haupham99a@gmail.com>
2020-11-29 00:42:55 +0700

    update tab notifications

--- facebook/lib/data/models/user_notification.dart
+++ facebook/lib/data/models/user_notification.dart
@@ -11,14 +11,14 @@ class UserNotification {
 }
 
 List<UserNotification> notifications = [
-  new UserNotification(imageUrl: 'assets/images/goalcast.png', content: 'Goalcast posted a new video', time: '3 hours ago'),
-  new UserNotification(imageUrl: 'assets/images/playstation.jpg', content: 'Playstation posted a new video', time: '8 hours ago'),
-  new UserNotification(imageUrl: 'assets/images/xbox.jpeg', content: 'Xbox posted a new video', time: '9 hours ago'),
-  new UserNotification(imageUrl: 'assets/images/reddit.png', content: 'Reddit posted a new video', time: '22 hours ago'),
-  new UserNotification(imageUrl: 'assets/images/linkedIn.jpg', content: 'Linkedin posted a new video', time: '1 day ago'),
-  new UserNotification(imageUrl: 'assets/images/goalcast.png', content: 'Goalcast posted a new video', time: '4 days ago'),
-  new UserNotification(imageUrl: 'assets/images/reddit.png', content: 'Reddit posted a new video', time: '6 days ago'),
-  new UserNotification(imageUrl: 'assets/images/xbox.jpeg', content: 'Xbox posted a new video', time: '1 week ago'),
-  new UserNotification(imageUrl: 'assets/images/linkedIn.jpg', content: 'Linkedin posted a new video', time: '3 weeks ago'),
-  new UserNotification(imageUrl: 'assets/images/playstation.jpg', content: 'Playstation posted a new video', time: '1 month ago')
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '3 hours ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '8 hours ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '9 hours ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '22 hours ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '1 day ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '4 days ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '6 days ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '1 week ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '3 weeks ago'),
+  new UserNotification(imageUrl: 'assets/images/fb.png', content: 'Dương Hoa', time: '1 month ago')
 ];
